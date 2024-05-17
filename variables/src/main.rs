d4b6535764d8836e91858929a01fae406ab4e88f
messatrt <essaessa9068@gmail.com>
2024-05-17 21:38:11 +0530

    Learne variables

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,8 +1,7 @@
 fn main() {
-    let x = 5;
     let x = 6;
     {
-        let x = x*2;
+        let x = x * 2;
         println!("The value of x is:{x}");
     }
     println!("The value of x is:{x}")
